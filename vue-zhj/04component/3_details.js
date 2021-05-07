5cc128a87672197fdf2164f78b747a5379334425
huahudie511 <82438491+huahudie511@users.noreply.github.com>
2021-05-07 10:24:09 +0800

    Update 3_details.js

--- vue-zhj/04component/3_details.js
+++ vue-zhj/04component/3_details.js
@@ -3,8 +3,16 @@ var Details={
 
   template:`<div style="background-color:#FF7F27">
 
-    <h1>这里是详情页</h1>    
-
+    <h1>这里是商品编号为{{lid}}的详情页</h1>    
+    <button @click="back">返回首页</button>
   </div>`, 
-
+  props:["lid"],
+  methods:{
+     back(){
+       this.$router.push("/");
+     }
+  },
+ mounted(){
+    console.log(this.lid)
+ }
 }
